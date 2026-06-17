b59eeed6b3ac494840dc6a27c96efc079e2c84af
Hardip <parmarhardip1995@gmail.com>
2026-06-17 05:20:14 +0000

    Fix: Remove redundant information from README about the build command [synced and rebranded]

--- assets/js/react-core.asset.php
+++ assets/js/react-core.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives'), 'version' => 'ebec371f9598e1cc1a3a');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives'), 'version' => '7d06637f989e1d92b90d');
